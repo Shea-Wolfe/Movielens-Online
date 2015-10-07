0a7d97ffbc420b4beb034d0e9999bbaad40cc164
William Butts <wbutts1023@gmail.com>
2015-10-07 00:20:37 -0400

    added __str__ methods, built out raters

--- movieratings/movie_data/models.py
+++ movieratings/movie_data/models.py
@@ -2,12 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Rater(models.Model):
-    gender = models.CharField(max_length=1)
+    male = 'm'
+    female = 'f'
+    gender_choices = [(male,'Male'),(female,'Female')]
+    gender = models.CharField(max_length=1,choices=gender_choices,default=male)
+
+    def __str__(self):
+        return 'user #{}'.format(self.id)
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=25)
+
+    def __str__(self):
+        return 'Movie #{}, {}'.format(self.id,self.title)
+
 
 class Rating(models.Model):
     rater = models.ForeignKey(Rater)
     movie = models.ForeignKey(Movie)
     score = models.IntegerField()
+
+    def __str__(self):
+        return '{} gives {} a {}'.format(self.rater,self.movie,self.score)

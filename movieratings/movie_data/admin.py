e5483ea2ced4de99aca73dbc746deb00e121c218
William Butts <wbutts1023@gmail.com>
2015-10-07 01:03:33 -0400

    added admin displays

--- movieratings/movie_data/admin.py
+++ movieratings/movie_data/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Movie,Rating,Rater
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['rater','movie','score']
+
+
+class RatingRater(admin.ModelAdmin):
+    list_display = ['id', 'gender']
+
 
 # Register your models here.
+admin.site.register(Movie,MovieAdmin)
+admin.site.register(Rating,RatingAdmin)
+admin.site.register(Rater,RatingRater)

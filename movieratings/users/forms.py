9886339d6451c9d36d2fc3672df28834c2a2ab35
William Butts <wbutts1023@gmail.com>
2015-10-08 20:34:05 -0400

    Added a very basic registration, rater object has preset values

---
+++ movieratings/users/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from movie_data.models import Rater
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+
+class RaterForm(forms.ModelForm):
+    class Meta:
+        model = Rater
+        fields = ('gender','age','occupation','zip_code')
--- movieratings/users/views.py
+++ movieratings/users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
+
+from .forms import UserForm, RaterForm
+from movie_data.models import Rater
+
 # Create your views here.
 def user_login(request):
     if request.method == 'POST':
@@ -25,3 +29,31 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            password = user.password
+
+            user.set_password(password)
+            user.save()
+
+            rater = Rater(
+                user=user,
+                gender='m',
+                age='25',
+                occupation='1'
+            )
+            rater.save()
+
+            user = authenticate(username=user.username,
+                                password=password)
+            login(request, user)
+            return redirect('user_page',user_id=rater.pk)
+    else:
+        form = UserForm()
+    return render(request, 'users/register.html',
+                  {'form': form})
